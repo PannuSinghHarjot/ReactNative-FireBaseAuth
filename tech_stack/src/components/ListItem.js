c4b99c3e297054d1026c7eb0aee12517a07f97ca
Harjot Singh <harjotsingh@Harjots-MacBook-Pro.local>
2018-06-03 20:03:06 -0700

    testing

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -5,59 +5,40 @@ import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
- 
-  renderDescription() {
-    const { library, selectedLibraryId } = this.props;  
-    if (library.id === selectedLibraryId) {
-      return (
-        <Text> 
-          {library.description}
-        </Text>
-      );
+    renderDescription() {
+        const { library, selectedLibraryId } = this.props;
+        if (library.id === selectedLibraryId) {
+           return (
+           <Text>{library.description}</Text>
+           );
+        }
     }
-  }
- 
-  render() {
-   const { title, id } = this.props.library;
-    return (
-      <TouchableWithoutFeedback 
-        onPress={() => this.props.selectLibrary(id)}
-      >
-        <View>
-          <CardSection>
-            <Text style={styles.titleStyles}> {title} </Text>
-          </CardSection>
-            {this.renderDescription()}
-        </View>
-     </TouchableWithoutFeedback>
-    );
-  }
+    render() {
+        const { titleStyle } = styles;
+        const { title, id } = this.props.library;
+        return (
+            <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
 }
 
 const styles = {
-  titleStyles: {
-    fontSize: 18,
-    paddingLeft: 15
-  }
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
 };
 
-const mapStateToProps = state => ({ selectLibraryId: state.selectedLibraryId });
-
-
+const mapStateToProps = state => {
+    return { selectedLibraryId: state.selectedLibraryId };
+};
 export default connect(mapStateToProps, actions)(ListItem);
-
-
-// import React from 'react';
-// import { Text } from 'react-native';
-// import { CardSection } from './common';
-
-// const ListItem = ({ library }) => {
-// const { title, description } = library;
-// return (
-  
-//           <CardSection>
-//             <Text> {description} </Text>
-//            </CardSection>
-//     );
-//   };
-// export default ListItem;
